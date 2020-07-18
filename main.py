8bc282d43152fa2eba13be3676cbb321a4a5955e
MertLightz <49613414+MertLightz@users.noreply.github.com>
2020-07-19 00:59:45 +0100

    Update main.py
    Add:
    'start_menu'

--- main.py
+++ main.py
@@ -15,4 +15,14 @@ def welcome():
     print('[' + '=' * 50 + ']')
     print('[{:^50}]'.format('welcome'))
     print('[' + '=' * 50 + ']')
+    print()
+    return
+
+def start_menu():
+    print('[' + '=' * 50 + ']')
+    print('[{:^50}]'.format('1 - login   '))
+    print('[{:^50}]'.format('2 - register'))
+    print('[{:^50}]'.format('3 - quit    '))
+    print('[' + '=' * 50 + ']')
+    print()
     return
