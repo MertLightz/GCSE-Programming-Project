0e3b807ade8b2b54556c74db9055a912a1bfa78d
MertLightz <49613414+MertLightz@users.noreply.github.com>
2020-07-26 20:14:29 +0300

    Update main.py
    Added:
    Fully working 'play'

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 #GCSE Programming Project
-
+import time
 import random
+global input_username
 
 ########################Used Globally########################
 
 def enter_to_continue():
     print('[' + '=' * 50 + ']')
+    time.sleep(0.1)
     print('[{:^50}]'.format('press enter to continue'))
+    time.sleep(0.1)
     print('[' + '=' * 50 + ']')
+    time.sleep(0.1)
 
     input()
     return
@@ -19,15 +23,22 @@ def blank_page():
 
 def message(abc):
     print('[' + '=' * 50 + ']')
+    time.sleep(0.1)
     print('[{:^50}]'.format(abc))
+    time.sleep(0.1)
     print('[' + '=' * 50 + ']')
+    time.sleep(0.1)
     return
 
 def welcome():
     print('[' + '=' * 50 + ']')
+    time.sleep(0.1)
     print('[{:^50}]'.format('welcome'))
+    time.sleep(0.1)
     print('[' + '=' * 50 + ']')
+    time.sleep(0.1)
     print()
+    time.sleep(0.1)
     return
 
 def get_option():
@@ -42,11 +53,17 @@ def get_option():
 
 def start_menu():
     print('[' + '=' * 50 + ']')
+    time.sleep(0.1)
     print('[{:^50}]'.format('1 - login   '))
+    time.sleep(0.1)
     print('[{:^50}]'.format('2 - register'))
+    time.sleep(0.1)
     print('[{:^50}]'.format('3 - quit    '))
+    time.sleep(0.1)
     print('[' + '=' * 50 + ']')
+    time.sleep(0.1)
     print()
+    time.sleep(0.1)
     return
 
 def start():
@@ -68,6 +85,7 @@ def start():
 ########################Used for Login#######################
 
 def login():
+    global input_username
     blank_page()
     valid = False
     
@@ -77,7 +95,9 @@ def login():
     print()
 
     input_username = input('username: ')
+    time.sleep(0.1)
     input_password = input('password: ')
+    time.sleep(0.1)
 
     with open('users.txt', 'r') as users:
         for each_line in users:
@@ -90,9 +110,13 @@ def login():
     blank_page()
     if valid == True:
         message('successfully logged in')
+        print()
+        enter_to_continue()
         main()
     else:
         message('invalid username or password')
+        print()
+        enter_to_continue()
         start()
 
 ########################Used for Register####################      
@@ -102,12 +126,17 @@ def register():
 
     message('register')
     print()
+    time.sleep(0.1)
     message('enter username and password')
     print()
+    time.sleep(0.1)
 
     input_username = input('username: ')
+    time.sleep(0.1)
     input_password = input('password: ')
+    time.sleep(0.1)
     print()
+    time.sleep(0.1)
 
     message('confirm password')
     print()
@@ -124,23 +153,34 @@ def register():
             if new_line[0] == input_username:
                 blank_page()
                 message('username is taken')
+                print()
+                enter_to_continue()
                 start()
     
     with open('users.txt', 'a') as users:
         users.write(input_username + ',' + input_password + '\n')
 
+    blank_page()
     message('registered')
+    print()
+    enter_to_continue()
     start()
 
 ########################Used for Main Menu###################
 
 def main_menu():
     print('[' + '=' * 50 + ']')
+    time.sleep(0.1)
     print('[{:^50}]'.format('1 - play game  '))
+    time.sleep(0.1)
     print('[{:^50}]'.format('2 - rules      '))
+    time.sleep(0.1)
     print('[{:^50}]'.format('3 - leaderboard'))
+    time.sleep(0.1)
     print('[' + '=' * 50 + ']')
+    time.sleep(0.1)
     print()
+    time.sleep(0.1)
     return
 
 def main():
@@ -151,9 +191,9 @@ def main():
 
     if option == 1:
         play()
-    if option == 2:
+    elif option == 2:
         rules()
-    if option == 3:
+    elif option == 3:
         leaderboard()
     else:
         message('invalid option')
@@ -167,15 +207,24 @@ def rules():
     message('rules')
 
     print()
+    time.sleep(0.1)
     print('[' + '=' * 50 + ']')
+    time.sleep(0.1)
     print('[{:^50}]'.format('1 - you have 2 chances to guess  '))
+    time.sleep(0.1)
     print('[{:^50}]'.format('2 - correct 1st time   = 3 points'))
+    time.sleep(0.1)
     print('[{:^50}]'.format('3 - correct 2nd time   = 1 points'))
+    time.sleep(0.1)
     print('[{:^50}]'.format('4 - incorrect 2nd time = lose    '))
+    time.sleep(0.1)
     print('[' + '=' * 50 + ']')
+    time.sleep(0.1)
     print()
+    time.sleep(0.1)
 
     enter_to_continue()
+    main()
 
 ########################Used for Leaderboard#################
 
@@ -184,9 +233,13 @@ def leaderboard():
     message('leaderboard')
 
     print()
+    time.sleep(0.1)
     print('[' + '=' * 51 + ']')
+    time.sleep(0.1)
     print('[{:^25}|{:^25}]'.format('name', 'score'))
+    time.sleep(0.1)
     print('[' + '=' * 51 + ']')
+    time.sleep(0.1)
     
     empty_list = []
     with open('leaderboard.txt', 'r') as leaderboard:
@@ -200,15 +253,22 @@ def leaderboard():
 
     for eachScore in show:
         print("[{:^25}|{:^25}]".format(eachScore[0], eachScore[1]))
+        time.sleep(0.1)
         print("[" + "=" * 51 + "]")
+        time.sleep(0.1)
         count = count + 1
         if count == 5:
             break
 
     print()
     enter_to_continue()
+    main()
 
 ########################Used for Game########################
+
+def add_to_leaderboard(input_username, points):
+    with open('leaderboard.txt', 'a') as leaderboard:
+        leaderboard.write(input_username + ',' + str(points) + '\n')
 
 def get_number_of_songs():
     number_of_songs = 0
@@ -220,7 +280,7 @@ def get_number_of_songs():
 def get_new_song(number_of_songs,already_selected):
     random_song = random.randint(0, number_of_songs - 1)
     while random_song in already_selected:
-        random_song = random.randint(0, number_of_songs)
+        random_song = random.randint(0, number_of_songs - 1)
 
     with open('songs.txt', 'r') as songs:
         for each_line in songs:
@@ -228,11 +288,10 @@ def get_new_song(number_of_songs,already_selected):
             if int(new_line[0]) == random_song:
                 current_artist = new_line[1]
                 current_song = new_line[2]
+                hidden_song = new_line[3]
                 break
-    return random_song,current_artist,current_song
-
-def get_points():
-    pass
+    already_selected.append(random_song)
+    return current_artist,current_song,hidden_song
 
 def play():
     already_selected = []
@@ -244,14 +303,70 @@ def play():
 
     number_of_songs = get_number_of_songs()
 
-    random_song,current_artist,current_song = get_new_song(number_of_songs,already_selected)
-    
-    already_selected.append(random_song)
-    
-    points = get_points()
-    
-    print('[' + '=' * 51 + ']')
-    print('[{:^25}|{:^25}]'.format(current_artist, current_song))
-    print('[' + '=' * 51 + ']')
+    new_song = True
+    check = False
+    while check == False:
+        if number_of_songs == len(already_selected):
+            message('you have beaten the game')
+            add_to_leaderboard(input_username,points)
+            main()
+            
+        if new_song == True:
+            current_artist,current_song,hidden_song = get_new_song(number_of_songs,already_selected)
 
-play()
+        for i in range(0,2):
+            blank_page()
+            
+            print('[' + '=' * 51 + ']')
+            time.sleep(0.1)
+            print('[{:^25}|{:^25}]'.format('points', points))
+            time.sleep(0.1)
+            print('[' + '=' * 51 + ']')
+            time.sleep(0.1)
+            print()
+            time.sleep(0.1)
+            
+            print('[' + '=' * 51 + ']')
+            time.sleep(0.1)
+            print('[{:^25}|{:^25}]'.format(current_artist, hidden_song))
+            time.sleep(0.1)
+            print('[' + '=' * 51 + ']')
+            time.sleep(0.1)
+            print()
+            time.sleep(0.1)
+
+            player_answer = input('Song Name: ').lower()
+            blank_page()
+            current_song = current_song.lower()
+            
+            if player_answer == current_song and i == 0:
+                message('correct, you gained 3 points')
+                print()
+                enter_to_continue()
+                points += 3
+                i = 0
+                break
+            elif player_answer == current_song and i == 1:
+                message('correct, you gained 1 points')
+                print()
+                enter_to_continue()
+                points += 1
+                i = 0
+                new_song = True
+            elif player_answer != current_song and i == 1:
+                message('incorrect, you have lost')
+                print()
+                check = True
+            else:
+                message('incorrect')
+                print()
+                enter_to_continue()
+                new_song = False
+
+    message('adding to leaderboard')
+    print()
+    add_to_leaderboard(input_username, points)
+    enter_to_continue()
+    main()
+
+start()
